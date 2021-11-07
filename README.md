9f560da2a205d15743f74188a543c786a7031932
CriDevs <CristianDAntonioDevs@gmail.com>
2021-11-07 16:12:08 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # nodets-animals
+
+Projeto feito no modulo do curso Node + Typescript
+
+
+### Pre-requisitos globais : 
+
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+
+npm install
+
+### para rodar o projeto
+
+npm run start-dev
---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import {Router} from 'express';
+import * as PageControler from '../controlers/pageControler';
+import * as SearchControler from '../controlers/searchControler';
+
+const router = Router();
+
+router.get('/', PageControler.home);
+router.get('/dogs', PageControler.dogs);
+router.get('/cats', PageControler.cats);
+router.get('/fishes', PageControler.fishes);
+
+router.get('/search', SearchControler.search)
+
+export default router;
